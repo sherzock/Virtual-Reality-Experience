559a30fc56e34824d1257ace597d098003e66a22
sherzock <jacobo.galofre@gmail.com>
2022-09-01 16:02:45 +0200

    Now Earth can be draged and rotated with mouse

--- Virtual Reality Experience/Assets/Scripts/EarthRotationandMovement.cs
+++ Virtual Reality Experience/Assets/Scripts/EarthRotationandMovement.cs
@@ -9,6 +9,9 @@ public class EarthRotationandMovement : MonoBehaviour
     public bool IsRotating = true;
     public float RotationSpeed = 0.1f;
 
+    Vector3 mPrevPos = Vector3.zero;
+    Vector3 mPosDelta = Vector3.zero;
+
 
     // Start is called before the first frame update
     void Start()
@@ -20,13 +23,26 @@ public class EarthRotationandMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (IsRotating)
-            Rotate();
+        //if (IsRotating)
+        //    AutoRotate();
+
+        GrabRotate();
     }
 
 
-    private void Rotate()
+    private void AutoRotate()
     {
         this.transform.Rotate(0, RotationSpeed, 0);
+    }
+
+    private void GrabRotate()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            mPosDelta = Input.mousePosition - mPrevPos;
+            transform.Rotate(transform.up, -Vector3.Dot(mPosDelta, Camera.main.transform.right), Space.World);
+            transform.Rotate(Camera.main.transform.right, Vector3.Dot(mPosDelta, Camera.main.transform.up), Space.World);
+        }
+        mPrevPos = Input.mousePosition;
     }
 }
